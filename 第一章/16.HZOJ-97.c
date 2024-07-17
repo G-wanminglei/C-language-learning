ed60937fa0a175fd72ec99518b4982756f30bad3
G-wanminglei <wanminglei@hotmail.com>
2024-07-17 12:09:39 +0800

    第一章代码

---
+++ 第一章/16.HZOJ-97.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 16.HZOJ-97.c
+	> Author:wanminglei 
+	> Mail:wanminglei@hotmail.com 
+	> Created Time: Tue 16 Jul 2024 09:47:59 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    long long sum, n;
+    scanf("%lld", &n);
+    printf("%lld\n", sum = n * (n + 1) / 2);
+    return 0;
+}
---
+++ 第一章/17.HZOJ-97-2.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 16.HZOJ-97-2.c
+	> Author:wanminglei 
+	> Mail:wanminglei@hotmail.com 
+	> Created Time: Tue 16 Jul 2024 09:53:29 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%lld\n", n * (n + 1ll) / 2);
+    return 0;
+}
