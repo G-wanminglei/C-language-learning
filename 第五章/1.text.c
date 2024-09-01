9913031804af8a83805a04d486a2803da875ce54
G-wanminglei <wanminglei@hotmail.com>
2024-09-01 23:12:33 +0800

    第五章代码

---
+++ 第五章/1.text.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 1.text.c
+	> Author:wanminglei 
+	> Mail:wanminglei@hotmail.com 
+	> Created Time: Sun 01 Sep 2024 10:26:58 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
