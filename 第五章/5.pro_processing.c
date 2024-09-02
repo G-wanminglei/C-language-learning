d32bbd68d70da1ba68d2be2afe3a73b3aa203636
G-wanminglei <wanminglei@hotmail.com>
2024-09-02 22:51:16 +0800

    第五章代码

---
+++ 第五章/5.pro_processing.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 5.pro_processing.c
+	> Author:wanminglei 
+	> Mail:wanminglei@hotmail.com 
+	> Created Time: Mon 02 Sep 2024 09:29:26 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
