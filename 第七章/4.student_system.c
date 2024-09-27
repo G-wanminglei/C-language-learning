a1fe5bb8270df7ff3905842f7a42004490348172
G-wanminglei <wanminglei@hotmail.com>
2024-09-27 23:50:59 +0800

    第七章代码

--- 第七章/4.student_system.c
+++ 第七章/4.student_system.c
@@ -8,14 +8,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-enum NO_TYPE {
-    LIST = 1,
-    ADD,
-    MODIFY,
-    DELETE,
-    QUIT
-};
-
+//定义结构体类型；
 typedef struct Student {
     char name[20];
     int age;
@@ -23,12 +16,16 @@ typedef struct Student {
     double height;
 } Student;
 
+//定义常量字符指针指向本地文件
 const char *file_name =  "student_data.txt";
+//定义宏（结构体数组大小）；
 #define MAX_N 10000
-
+//定义结构体数组储存文件数据（程序的数据结构）；
 Student stu_arr[MAX_N];
+//定义整型对数组元素计数；
 int stu_cut = 0;
 
+//定义函数从文件读取数据至数组；
 int read_from_file(Student *arr) {
     int i = 0;
     FILE *fp = fopen(file_name, "r");
@@ -43,6 +40,43 @@ int read_from_file(Student *arr) {
     return i;
 }
 
+//定义函数从数组写入数据至文件；
+void output_to_file(Student *arr, int n) {
+    FILE *fp = fopen(file_name, "a");
+    for (int i = 0; i < n; i++) {
+        fprintf(fp, "%s %d %d %.2lf\n",
+            arr[i].name, arr[i].age,
+            arr[i].class, arr[i].height
+            );
+        fclose(fp);
+    }
+    return ;
+}
+
+//定义清理文件数据函数；
+void clear_file() {
+    FILE *fp = fopen(file_name, "w");
+    fclose(fp);
+    return ;
+}
+
+//定义刷新文件数据函数（先清理再重新写入）；
+void restor_data_to_file(Student *arr, int n) {
+    clear_file();
+    output_to_file(arr, n);
+    return ;
+}
+
+//定义枚举变量，增加可读性；
+enum NO_TYPE {
+    LIST = 1,
+    ADD,
+    MODIFY,
+    DELETE,
+    QUIT
+};
+
+//定义交互界面函数；
 int usage() {
     int no;
     do {
@@ -57,6 +91,7 @@ int usage() {
     return no;
 }
 
+//定义查询数据函数；
 void list_students(Student *arr) {
     int len = printf("%4s|%10s|%4s|%6s|%8s|", 
         "ID", "Name", "Age", "Class", "Hegiht"
@@ -73,15 +108,73 @@ void list_students(Student *arr) {
     return ;
 }
 
+//定义增加数据函数；
+void add_a_student(Student *arr) {
+    printf("add new item : (name, age, class, height)\n");
+    printf("mysql > ");
+    scanf("%s%d%d%lf", 
+        arr[stu_cut].name, 
+        &arr[stu_cut].age, 
+        &arr[stu_cut].class, 
+        &arr[stu_cut].height
+    );
+    output_to_file(stu_arr + stu_cut, 1);
+    stu_cut += 1;
+    printf("add a new student success\n");
+    return ;
+}
+
+//定义修改数据函数；
+void modify_a_student(Student *arr) {
+    int id;
+    do {
+        printf("modify id : ");
+        scanf("%d", &id);
+    } while (id < 0 || id >= stu_cut);
+    printf("add new item : (name, age, class, height)\n");
+    printf("mysql > ");
+    scanf("%s%d%d%lf", 
+        arr[id].name, 
+        &arr[id].age, 
+        &arr[id].class, 
+        &arr[id].height
+    );
+    restor_data_to_file(arr, stu_cut);
+    return ;
+}
+
+//定义删除文件数据函数；
+void delete_a_student(Student *arr) {
+    if (stu_cut == 0) {
+        printf("there is no data\n");
+        return ;
+    }
+    list_students(arr);
+    int id;
+    do {
+        printf("delete id : ");
+        scanf("%d", &id);
+    } while (id < 0 || id > stu_cut);
+    char s[100];
+    printf("confim (y / n) : ");
+    scanf("%s", s);
+    if (s[0] != 'y') return;
+    for (int i = id + 1; i < stu_cut; i++) {
+        arr[i - 1] = arr[i];
+    }
+    stu_cut -= 1;
+    restor_data_to_file(arr, stu_cut);
+}
+
 int main() {
     stu_cut = read_from_file(stu_arr);
     while(1) {
         int no = usage();
         switch (no) {
             case LIST: list_students(stu_arr); break;
-            case ADD: printf("add_\n"); break;
-            case MODIFY: printf("modify_\n"); break;
-            case DELETE: printf("delete_\n"); break;
+            case ADD: add_a_student(stu_arr); break;
+            case MODIFY: modify_a_student(stu_arr); break;
+            case DELETE: delete_a_student(stu_arr); break;
             case QUIT: printf("quit\n"); exit(0);
         }
     }
