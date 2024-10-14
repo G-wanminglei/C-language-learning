74fa663ec4a9e16e09a4818498071934f0cec81a
G-wanminglei <wanminglei@hotmail.com>
2024-10-14 21:41:56 +0800

    第七章代码

--- 第七章/13.projest/include/database.h
+++ 第七章/13.projest/include/database.h
@@ -8,6 +8,9 @@
 #ifndef _DATABASE_H
 #define _DATABASE_H
 
+//1.界面；
+
+//定义枚举变量表示用户界面；
 enum OP_TYPE {
     CHOOSE_TABLE,
     TABLE_USAGE,
@@ -18,7 +21,33 @@ enum OP_TYPE {
     DELETE_TABLE
 };
 
+//2.表信息；
+
+//定义结构体类型表示相关表信息；
+struct Database {
+    const char *table_name;
+};
+
+//声明结构体类型变量db存储相关信息；
+extern struct Database db;
+
+//db初始化函数指针；
+typedef void (*InitTable_T)(struct Database *);
+
+//3.表库；
+
+//声明表库结构体类型；
+struct TableInfo {
+    const char *table_name;
+    InitTable_T init_table;
+};
+
+//声明注册函数；(将表信息写入表库)
+void register_table(const char *, InitTable_T);
+
+//4.流程；
+
+//声明程序入口函数；
 void run_database();
-void register_table(const char *);
 
 #endif
--- 第七章/13.projest/src/database.c
+++ 第七章/13.projest/src/database.c
@@ -8,18 +8,25 @@
 #include<stdio.h>
 #include<database.h>
 
-const char *tables[100];
+//定义表库及表数量变量；
+struct TableInfo tables[100];
 int table_cnt = 0;
 
-void register_table(const char *table_name) {
-    tables[table_cnt] = table_name;
+//定义结构体变量存储表信息；
+struct Database db;
+
+//注册表信息函数；
+void register_table(const char *table_name, InitTable_T init_table) {
+    tables[table_cnt].table_name = table_name;
+    tables[table_cnt].init_table = init_table;
     table_cnt += 1;
     return ;
 }
 
+//选择表函数；
 static enum OP_TYPE choose_table() {
     for (int i = 0; i < table_cnt; i++) {
-        printf("%d : %s\n", i, tables[i]);
+        printf("%d : %s\n", i, tables[i].table_name);
     }
     printf("%d : quit\n", table_cnt);
     int x;
@@ -27,15 +34,19 @@ static enum OP_TYPE choose_table() {
         printf("input : ");
         scanf("%d", &x);
     } while (x < 0 || x > table_cnt);
-    if (x == 1) return TABLE_USAGE;
+    if (x < table_cnt) {
+        tables[x].init_table(&db);
+        return TABLE_USAGE;
+    }
     else return OP_END;
 }
 
+//操作表函数；
 static enum OP_TYPE table_usage() {
-    printf("1 : list table\n");
-    printf("2 : add an item to table\n");
-    printf("3 : modify an item in table\n");
-    printf("4 : delete an item  table\n");
+    printf("1 : list %s\n", db.table_name);
+    printf("2 : add an item to %s\n", db.table_name);
+    printf("3 : modify an item in %s\n", db.table_name);
+    printf("4 : delete an item  %s\n", db.table_name);
     printf("5 : back\n");
     int x;
     do {
@@ -49,26 +60,31 @@ static enum OP_TYPE table_usage() {
     else return CHOOSE_TABLE;
 }
 
+//查询表信息函数；
 static enum OP_TYPE list_table() {
     printf("list table\n");
     return TABLE_USAGE;
 };
 
+//增加表信息函数；
 static enum OP_TYPE add_table() {
     printf("add table\n");
     return TABLE_USAGE;
 };
 
+//修改表信息函数；
 static enum OP_TYPE modify_table() {
     printf("modify table\n");
     return TABLE_USAGE;
 };
 
+//删除表信息函数；
 static enum OP_TYPE delete_table() {
     printf("delete table\n");
     return TABLE_USAGE;
 };
 
+//用户界面选择函数；
 enum OP_TYPE run(enum OP_TYPE status) {
     switch (status) {
         case CHOOSE_TABLE: {
@@ -96,6 +112,7 @@ enum OP_TYPE run(enum OP_TYPE status) {
     return OP_END;
 }
 
+//程序入口定义；
 void run_database() {
     enum OP_TYPE status = CHOOSE_TABLE;
     while (1) {
